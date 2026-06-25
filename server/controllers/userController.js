b040e2f41d2273947c2b572421d97c467899945f
Arya <arya14728@gmail.com>
2026-06-25 20:01:07 +0530

    Added the routes for userControl and resumeControl, middleware

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import bycrpt from "bcrypt";
 import User from "../models/User.js";
 import jwt from 'jsonwebtoken'
+import Resume from "../models/Resume.js";
 
 const generateToken=(userId)=>{
     const token=jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn:'7d'})
@@ -9,7 +10,6 @@ const generateToken=(userId)=>{
 
 //Controller for user registration
 //POST: api/users/register
-
 export const registerUser = async (req,res)=>{
     try {
         const {name,email,password}=req.body;
@@ -35,11 +35,7 @@ export const registerUser = async (req,res)=>{
 
         newUser.password = undefined;
 
-        return res.status(201).json({
-            message: 'User created successfully',
-            token,
-            user: newUser
-        })
+        return res.status(201).json({ message: 'User created successfully', token, user: newUser})
     } catch (error) {
         return res.status(400).json({message: error.message})
     }
@@ -47,8 +43,7 @@ export const registerUser = async (req,res)=>{
 
 
 //Controller for user login
-//POST: api/users/login
-
+//GET: api/users/login
 export const loginUser = async (req,res)=>{
     try {
         const {email,password}=req.body;
@@ -71,11 +66,35 @@ export const loginUser = async (req,res)=>{
         const token = generateToken(User._id)
         User.password = undefined;
 
-        return res.status(200).json({
-            message: 'Login successfully',
-            token,
-            user
-        })
+        return res.status(200).json({message: 'Login successfully', token, user})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+//Controller for getting user by id
+//GET: api/users/data
+export const getUserById = async (req,res)=>{
+    try {
+        const userId=req.userId
+        const user=await User.findById(userId)
+        if(!user){
+            return res.status(404).json({message:'User not found'})
+        }
+        user.password=undefined
+        return res.status(200).json({user})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+}
+
+//Controller for getting resume by id
+//GET: api/users/resumes
+export const getUserResumes = async (req,res)=>{
+    try {
+        const userId=req.userId
+        const resumes=await Resume.find({userId})
+        return res.status(200).json({resumes})
     } catch (error) {
         return res.status(400).json({message: error.message})
     }
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js"
+import userRouter from "./routes/userRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
 
 const app=express()
 const PORT=process.env.PORT || 3000
@@ -16,6 +18,8 @@ app.get('/',(req,res)=>{
     res.send("Server is live");
     console.log("Home");
 })
+app.use('/api/users',userRouter)
+app.use('/api/resumes',resumeRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server running on ${PORT}`);
